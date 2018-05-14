bdeb6e5458bed7ebef59650ac248d6bf6e61b65c
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-14 20:57:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
         "I LOVE YOU TOO PUMPKIN!"
-  #elsif "#{phrase}" != phrase.upcase
+  elsif "#{phrase}" != phrase.upcase
     #"HUH?! SPEAK UP, SONNY!"
-    #elsif "#{phrase}" == phrase.upcase
+    else "#{phrase}" == phrase.upcase
       #"NO, NOT SINCE 1938!"
     end
 end
