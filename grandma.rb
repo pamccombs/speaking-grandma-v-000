f99261d9b8f2097e9e9380e37fbf5ef26d4814a3
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-14 20:57:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
-        "I LOVE YOU TOO PUMPKIN"
+        "I LOVE YOU TOO PUMPKIN!"
   #elsif "#{phrase}" != phrase.upcase
     #"HUH?! SPEAK UP, SONNY!"
     #elsif "#{phrase}" == phrase.upcase
