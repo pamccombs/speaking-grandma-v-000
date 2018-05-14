49001b9c5d7a58f0c9e008e38d05fb0b2971caee
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-14 20:51:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
+  elsif "#{phrase}" == phrase.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
